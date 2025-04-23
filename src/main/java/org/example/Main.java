956416772f5452a33d2d6941c5d528019158abef
Lev <lev.liv2@gmail.com>
2025-04-23 09:47:40 +0300

    arguments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Test ob = new Test();
+
+        int a = 15, b = 20;
+
+        System.out.println("a і b до виклику: "+ a + " "+ b);
+
+        ob.meth(a,b);
+
+        System.out.println("a i b після виклику: "+ a + " "+ b);
     }
 }
