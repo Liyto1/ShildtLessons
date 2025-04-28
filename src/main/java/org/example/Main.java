83d4d867c511dbe511e9845982ac7dd262f5aa56
Lev <lev.lvi2@gmail.com>
2025-04-28 18:18:23 +0300

    security modifications

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        RecTest ob = new RecTest(10);
-        int i;
+       Stack myStack1 = new Stack(10);
+       Stack myStack2 = new Stack(20);
 
-        for (i = 0; i < 10; i++) ob.values[i] = i;
+       for (int i = 0; i < 10; i++) myStack1.push(i);
+       for (int i = 0; i < 20; i++) myStack2.push(i);
 
-        ob.printArray(10);
+        System.out.println("Stack 1: " );
+        for (int i = 0; i < 10; i++) System.out.println(myStack1.pop());
+
+        System.out.println("Stack 2: " );
+        for (int i = 0; i < 20; i++) System.out.println(myStack2.pop());
+
     }
 }
---
+++ src/main/java/org/example/Stack.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Stack {
+    private int[]stack;
+    private int tos;
+
+    public Stack(int size) {
+        stack= new int[size];
+        tos = -1;
+    }
+
+    void push(int item){
+        if (tos == -9){
+            System.out.println("Stack is full");
+        }else{
+            stack[++tos] = item;
+        }
+    }
+
+    int pop(){
+        if (tos < 0){
+            System.out.println("Stack is empty");
+            return 0;
+        }else{
+            return stack[tos--];
+        }
+    }
+}
+
