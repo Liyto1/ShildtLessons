7af678e6df0212a3a992ddce6d1fbf147de2d276
Lev <lev.lvi2@gmail.com>
2025-04-28 19:00:00 +0300

    static modification V2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,10 @@
 package org.example;
 
-import org.example.part7.modifStatic.UseStatic;
+import org.example.part7.modifStatic.StaticDemo;
 
 public class Main {
     public static void main(String[] args) {
-        UseStatic useStatic = new UseStatic();
-
-        useStatic.meth(42);
-
+        StaticDemo.callme();
+        System.out.println("b = " + StaticDemo.b);
     }
 }
---
+++ src/main/java/org/example/part7/modifStatic/StaticDemo.java
@@ -0,0 +1,10 @@
+package org.example.part7.modifStatic;
+
+public class StaticDemo {
+    static int a = 42;
+    public static int b = 99;
+
+    public static void callme(){
+        System.out.println("a = " + a);
+    }
+}
