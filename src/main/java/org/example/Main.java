5e9a71b3de924f48ac0d0749f1dc058ba381b9ab
Lev <lev.liv2@gmail.com>
2025-04-29 14:12:53 +0300

    String examples

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,23 @@ import org.example.part7.innerClass.Outer;
 
 public class Main {
     public static void main(String[] args) {
-        Outer outer = new Outer();
-        outer.test();
+        String strOb1 = "First string";
+        String strOb2 = "Second string";
+        String strOb3 = strOb1;
+
+        System.out.println("length of strOb1: "+ strOb1.length());
+        System.out.println("character of strOb1 with index 3 : "+ strOb1.charAt(3));
+
+        if(strOb1.equals(strOb2)){
+            System.out.println("strOb1 equals strOb2");
+        }else{
+            System.out.println("strOb1 not equals strOb2");
+        }
+
+        if(strOb1.equals(strOb3)){
+            System.out.println("strOb1 equals strOb3");
+        }else{
+            System.out.println("strOb1 not equals strOb3");
+        }
     }
 }
