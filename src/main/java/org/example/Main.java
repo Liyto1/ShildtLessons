f32d3925f2b8ed895ed61698fd539c2f0f7e35ca
Lev <lev.liv2@gmail.com>
2025-04-29 09:47:26 +0300

    massif`s length

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,14 @@
 package org.example;
 
-import org.example.part7.modifStatic.StaticDemo;
 
 public class Main {
     public static void main(String[] args) {
-        StaticDemo.callme();
-        System.out.println("b = " + StaticDemo.b);
+        int[]a1 = new int[10];
+        int[]a2 = {3, 5, 7, 1, 8, 99, 44, -10};
+        int[]a3  ={4,3,2,1};
+
+        System.out.println("length of a1: " + a1.length);
+        System.out.println("length of a2: " + a2.length);
+        System.out.println("length of a3: " + a3.length);
     }
 }
